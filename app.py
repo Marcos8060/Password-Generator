a2d17687db100e45b286afc428f49e7dded50b5b
Marcos8060 <marcosgav80@gmail.com>
2021-11-17 14:37:40 +0300

    imported the string and random modules

---
+++ app.py
@@ -0,0 +1,20 @@
+import string
+import random
+
+def generate():
+    pass1 = string.ascii_uppercase
+
+    pass2 = string.ascii_lowercase
+
+    pass3 = string.digits
+ 
+    pass4 = string.punctuation
+
+    s = []
+    s.extend(list(pass1))
+    s.extend(list(pass2))
+    s.extend(list(pass3))
+    s.extend(list(pass4))
+    print(s)
+
+generate()
